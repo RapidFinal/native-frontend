877cecae495b056327d14b664ae7d9d875c4e9f2
Pan Teparak <panteparak@me.com>
2018-07-04 17:50:36 +0700

    RNF ios done

--- android/app/src/main/java/com/jobme/MainApplication.java
+++ android/app/src/main/java/com/jobme/MainApplication.java
@@ -5,6 +5,7 @@ import android.app.Application;
 import com.facebook.react.ReactApplication;
 import io.invertase.firebase.RNFirebasePackage;
 import io.invertase.firebase.RNFirebasePackage;
+import io.invertase.firebase.RNFirebasePackage;
 import com.facebook.reactnative.androidsdk.FBSDKPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -26,6 +27,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNFirebasePackage(),
             new RNFirebasePackage(),
             new RNFirebasePackage(),
             new FBSDKPackage()
