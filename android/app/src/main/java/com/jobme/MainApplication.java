fe622911c537bf3ada36070f6aff706139d1813e
Pan Teparak <panteparak@me.com>
2018-07-04 16:45:27 +0700

    FB Android setup

--- android/app/src/main/java/com/jobme/MainApplication.java
+++ android/app/src/main/java/com/jobme/MainApplication.java
@@ -3,6 +3,9 @@ package com.jobme;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import io.invertase.firebase.RNFirebasePackage;
+import io.invertase.firebase.RNFirebasePackage;
+import com.facebook.reactnative.androidsdk.FBSDKPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -22,7 +25,10 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
-          new MainReactPackage()
+          new MainReactPackage(),
+            new RNFirebasePackage(),
+            new RNFirebasePackage(),
+            new FBSDKPackage()
       );
     }
 
