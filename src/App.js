ce800cb6f6309c657da2d685008a687f256541fa
hart_suvanadat@hotmail.com <hart_suvanadat@hotmail.com>
2018-07-05 16:11:38 +0700

    Supress harmless (maybe) error from React-Navigator.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import React from 'react';
 import compose from 'recompose/compose'
 import PropTypes from 'prop-types'
 import {StyleSheet} from "react-native";
+import { YellowBox } from 'react-native';
+
+YellowBox.ignoreWarnings(['Warning: isMounted(...) is deprecated', 'Module RCTImageLoader']);
 
 class App extends React.Component {
 
