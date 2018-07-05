560e8175ac7ca86e5895e745f9e5a30a3b14eea8
tui95 <khemmatat.thean@gmail.com>
2018-07-05 20:43:21 +0700

    add stepper component to index

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,4 @@
-import AccountSidebar from ',/AccountSidebar'
+import AccountSidebar from './AccountSidebar'
 import AccountSidebarLogin from './AccountSidebarLogin'
 import AccountSidebarNotLogin from './AccountSidebarNotLogin'
 import Carousel from './Carousel'
@@ -13,6 +13,7 @@ import ProjectSection from './ProjectSection'
 import SearchCard from './ProjectSection'
 import SideMenu from './SideMenu'
 import StatusDropdown from './StatusDropdown'
+import Stepper from './Stepper'
 import Tag from './Tag'
 import TextField from './TextField'
 import TimelineProjectCard from './TimelineProjectCard'
@@ -34,6 +35,7 @@ export {
     SearchCard,
     SideMenu,
     StatusDropdown,
+    Stepper,
     Tag,
     TextField,
     TimelineProjectCard
