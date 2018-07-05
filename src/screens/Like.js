1fac9a8ddf8f84d1a38e4f5f8746dc8f6dad8026
hart_suvanadat@hotmail.com <hart_suvanadat@hotmail.com>
2018-07-06 00:40:01 +0700

    Basic functionality of like screen working with mockup data

--- src/screens/Like.js
+++ src/screens/Like.js
@@ -1,7 +1,12 @@
 import React from 'react';
 import compose from 'recompose/compose'
 import PropTypes from 'prop-types'
-import {FlatList, StyleSheet, Text, View} from "react-native";
+import {Button, FlatList, StyleSheet, Text, TouchableOpacity} from "react-native";
+import firebase from 'react-native-firebase';
+
+// TODO: Delete test_like & get the correct db.ref()
+const currentUser = "user1";
+const likeProfilesRef = firebase.database().ref('test_like/profile/' + currentUser + "/like_profiles");
 
 class Like extends React.Component {
 
@@ -9,17 +14,50 @@ class Like extends React.Component {
 
     }
 
+    constructor(props) {
+        super(props);
+        this.state = {
+            profiles : []
+        };
+    }
+
+    componentDidMount() {
+        likeProfilesRef.on('child_added', snapshot => {
+            let profile = { id: snapshot.key, content: snapshot.val()};
+            this.setState({ profiles: this.state.profiles.concat([profile])});
+        })
+    }
+
     render(){
         return (
             <FlatList
-                data={[{key: 'a'}, {key: 'b'}]}
-                renderItem={({item}) =>
-                    <View style={styles.horizontalProfileCard}>
-                        <Text style={styles.profileContentPlaceHolder}>Profile Text</Text>
-                    </View>}
+                data={this.state.profiles}
+                renderItem={({item, index}) =>
+                    <TouchableOpacity onPress={this.goToProfile} style={styles.horizontalProfileCard}>
+                        <Text style={styles.profileContentPlaceHolder}>{item.content}</Text>
+                        <Button
+                            title="Delete"
+                            onPress={this.removeLike.bind(this,index)}
+                            style={styles.deleteButton}
+                        />
+                    </TouchableOpacity>}
+                keyExtractor={item => item.id}
             />
         );
     }
+
+    goToProfile = () => {
+        this.props.navigation.navigate("View");
+    };
+
+    removeLike = (index) => {
+        const profilesClone = this.state.profiles.slice();
+        profilesClone.splice(index, 1);
+        likeProfilesRef.child(this.state.profiles[index].id).remove();
+        this.setState({
+            profiles: profilesClone
+        });
+    };
 
 }
 
@@ -33,8 +71,11 @@ const styles = StyleSheet.create({
         flexDirection: 'row',
         alignItems: 'center'
     },
+    deleteButton: {
+        width: '20%',
+    },
     profileContentPlaceHolder: {
-        width: '100%',
+        width: '80%',
         textAlign: 'center'
     }
 });
