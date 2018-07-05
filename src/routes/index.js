36be33d9a92cfc000b4f4310446f94401bef1d4a
hart_suvanadat@hotmail.com <hart_suvanadat@hotmail.com>
2018-07-05 17:11:12 +0700

    Add icons to tab bar (not tested on ios)

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,7 +3,9 @@ import Edit_Test from "../screens/Edit_Test";
 import Login_Test from "../screens/Login_Test";
 import View_Test from "../screens/View_Test";
 import Like_Test from "../screens/Like_Test";
+import Ionicons from 'react-native-vector-icons/Ionicons';
 import {createBottomTabNavigator, createSwitchNavigator} from "react-navigation";
+import React from "react";
 
 const EmployerTabStack = createBottomTabNavigator(
     {
@@ -11,6 +13,24 @@ const EmployerTabStack = createBottomTabNavigator(
         Home: Home_Test,
         Like: Like_Test
     },
+    {
+        navigationOptions: ({ navigation }) => ({
+            tabBarIcon: ({focused, tintColor}) => {
+                const {routeName} = navigation.state;
+                let iconName;
+                if (routeName === 'View') {
+                    iconName = `ios-contact${focused ? '' : '-outline'}`;
+                } else if (routeName === 'Home') {
+                    iconName = `ios-home${focused ? '' : '-outline'}`;
+                }
+                else if (routeName === 'Like') {
+                    iconName = `ios-heart${focused ? '' : '-outline'}`;
+                }
+
+                return <Ionicons name={iconName} size={25} color={tintColor}/>;
+            }
+        })
+    }
 );
 
 const CandidateTabStack = createBottomTabNavigator(
@@ -19,6 +39,24 @@ const CandidateTabStack = createBottomTabNavigator(
         Home: Home_Test,
         Edit: Edit_Test,
     },
+    {
+        navigationOptions: ({ navigation }) => ({
+            tabBarIcon: ({focused, tintColor}) => {
+                const {routeName} = navigation.state;
+                let iconName;
+                if (routeName === 'View') {
+                    iconName = `ios-contact${focused ? '' : '-outline'}`;
+                } else if (routeName === 'Home') {
+                    iconName = `ios-home${focused ? '' : '-outline'}`;
+                }
+                else if (routeName === 'Edit') {
+                    iconName = `ios-create${focused ? '' : '-outline'}`;
+                }
+
+                return <Ionicons name={iconName} size={25} color={tintColor}/>;
+            }
+        })
+    }
 );
 
 export default createSwitchNavigator(
