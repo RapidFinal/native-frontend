ab19c6a4b44b9f39552931756b35a274d80eaa24
Soraya Sakonwittayanon <soraya.sakonwittayanon@gmail.com>
2018-07-05 12:53:00 +0700

    list out the methods that could be used

---
+++ src/api/databaseService.js
@@ -0,0 +1,59 @@
+class DatabaseService {
+
+  // Employee
+
+  createEmployeeInfo(uid, value) {
+
+  }
+
+  getEmployeeInfo(uid) {
+
+  }
+
+  updateEmployeeInfoAt(uid, field, value) {
+
+  }
+
+  getEmployeeInfoAt(uid, field) {
+
+  }
+
+  // Employer
+
+  createEmployerInfo(uid, value) {
+
+  }
+
+  updateEmployerInfoAt(uid, field, value) {
+
+  }
+
+  getEmployerInfo(uid) {
+
+  }
+
+  getEmployerInfoAt(uid, field) {
+
+  }
+
+  // Categories
+
+  getCategories() {
+
+  }
+
+  getSubCategories(categoryId) {
+
+  }
+
+  // Tags
+
+  getSuggestedTagsFrom(categoryId) {
+
+  }
+
+  getEmployeeFromTag(tagId) {
+
+  }
+
+}
