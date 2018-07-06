82b554d6791c830916f2bbe2529cc9478be68744
tui95 <khemmatat.thean@gmail.com>
2018-07-06 22:32:23 +0700

    add native base picker

--- src/components/StatusDropdown.js
+++ src/components/StatusDropdown.js
@@ -1,7 +1,14 @@
 import React from 'react';
 import compose from 'recompose/compose'
 import PropTypes from 'prop-types'
-import {StyleSheet} from "react-native";
+import {StyleSheet, View} from "react-native";
+import {Item, Picker, Text} from "native-base";
+
+const status = [
+    "Looking for job",
+    "Looking for opportunities",
+    "Not looking"
+]
 
 class StatusDropdown extends React.Component {
 
@@ -10,13 +17,43 @@ class StatusDropdown extends React.Component {
     }
 
     state = {
+        selectedStatus: undefined
+    }
 
+    changeStatus = (status)=>{
+        if (status !== null) {
+            this.setState({
+                selectedStatus : status
+            });
+        }
     }
 
     render(){
         const {} = this.state;
         return (
-            null
+            <View>
+                <Text>Status</Text>
+                <Item picker regular>
+                    <Picker mode="dropdown"
+                            placeholder="Select your SIM"
+                            selectedValue={this.state.selectedStatus}
+                            onValueChange={this.changeStatus.bind(this)}
+                    >
+                        <Picker.Item
+                            label="Select Status..."
+                            value={null}/>
+                        {
+                            status.map((item,index)=>{
+                                return <Picker.Item
+                                    key={index}
+                                    label={item}
+                                    value={item}
+                                />;
+                            })
+                        }
+                    </Picker>
+                </Item>
+            </View>
         )
     }
 
