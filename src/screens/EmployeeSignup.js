92c7446bfb17838b9b027c06e6ee24d3c25186ee
tui95 <khemmatat.thean@gmail.com>
2018-07-06 21:06:09 +0700

    finish account info ui

--- src/screens/EmployeeSignup.js
+++ src/screens/EmployeeSignup.js
@@ -1,15 +1,39 @@
 import React from 'react';
 import compose from 'recompose/compose'
 import PropTypes from 'prop-types'
-import {StyleSheet, Text, View} from "react-native";
-import {Container, Content} from "native-base";
-import {Stepper, BackButton, NextButton} from "../components";
+import {StyleSheet, View} from "react-native";
+import {
+    Container,
+    Content,
+    Form,
+    Input,
+    Item,
+    Text,
+    H3,
+} from "native-base";
+import {
+    Stepper,
+    BackButton,
+    NextButton
+} from "../components";
 
+const TextInput = ({text}) => (
+    <View style={styles.textInput}>
+        <Text>{text}</Text>
+        <Item regular>
+            <Input placeholder={text} />
+        </Item>
+    </View>
+);
 
 const AccountInfo = () => (
-    <View>
-        <Text>Page 1</Text>
-    </View>
+    <Form style={styles.form}>
+        <TextInput text={"First name"} />
+        <TextInput text={"Last name"} />
+        <TextInput text={"Email"} />
+        <TextInput text={"Password"} />
+        <TextInput text={"Confirm Password"} />
+    </Form>
 );
 
 const SelectCategory = () => (
@@ -17,10 +41,12 @@ const SelectCategory = () => (
         <Text>Page 2</Text>
     </View>);
 
-const EmployeeInfo = () => (
-    <View>
-        <Text>Page 3</Text>
-    </View>);
+const EmployeeInfo = ({selectedSkill}) => (
+    <Form>
+        <H3>Your top skills</H3>
+        <Text>Suggestion of popular skills for {selectedSkill}</Text>
+    </Form>
+);
 
 const WorkExp = () => (
     <View>
@@ -56,7 +82,7 @@ class EmployeeSignup extends React.Component {
             case 1:
                 return <SelectCategory/>;
             case 2:
-                return <EmployeeInfo/>;
+                return <EmployeeInfo selectedSkill={"Design"}/>;
             case 3:
                 return <WorkExp/>
         }
@@ -68,11 +94,15 @@ class EmployeeSignup extends React.Component {
         const backButton = <BackButton onPress={this.prevStep}/>;
         switch (this.state.currentStep) {
             case 0:
-                return nextButton;
+                return (
+                    <View style={styles.nextButton}>
+                        {nextButton}
+                    </View>
+                );
             case 1:
             case 2:
                 return (
-                    <View>
+                    <View style={styles.buttons}>
                         {backButton}
                         {nextButton}
                     </View>
@@ -87,13 +117,15 @@ class EmployeeSignup extends React.Component {
         const stepCount = 4;
         return (
             <Container>
-                <Content>
+                <Content style={styles.content}>
                     <Stepper
                         currentPosition={this.state.currentStep}
                         stepCount={stepCount}
                     />
-                    {this.displayStep()}
-                    {this.displayButtons()}
+                    <View style={styles.form}>
+                        {this.displayStep()}
+                        {this.displayButtons()}
+                    </View>
                 </Content>
             </Container>
         )
@@ -102,7 +134,24 @@ class EmployeeSignup extends React.Component {
 }
 
 const styles = StyleSheet.create({
-
+    content: {
+        marginTop: 25,
+        marginHorizontal: 50
+    },
+    form: {
+        marginTop: 10
+    },
+    textInput: {
+        marginBottom: 10
+    },
+    nextButton: {
+        alignSelf: 'flex-end',
+        marginTop: 10
+    },
+    buttons: {
+        flexDirection: 'row',
+        justifyContent: 'space-between'
+    }
 });
 
 export default compose() (EmployeeSignup)
