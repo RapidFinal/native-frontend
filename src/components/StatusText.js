4b0162ec06a0618ae8da71ba6d47f6742b9937f7
ice48623 <ice48623@gmail.com>
2018-07-10 01:02:06 +0700

    update status, add description

---
+++ src/components/StatusText.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import compose from 'recompose/compose'
+import PropTypes from 'prop-types'
+import {StyleSheet, Image, View, Text} from "react-native";
+
+class StatusText extends React.Component {
+
+    static propTypes = {
+
+    }
+
+    state = {
+
+    }
+
+    render(){
+        // const {} = this.state; // to easily access state put desire variable in the curly brace so it may become const {variable} = this.state;
+        return (
+            <View style={styles.StatusBox}>
+                <Text style={styles.StatusText}>{this.props.status}</Text>
+                <Image source={require('../assets/images/green.png')} style={styles.StatusImg}/>
+            </View>
+        )
+    }
+
+}
+
+const styles = StyleSheet.create({
+    StatusBox: {
+        marginTop: 15,
+        maxWidth: 400,
+        maxHeight: 50,
+        borderWidth: 1,
+        borderColor: '#CCC',
+        flex: 1,
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+        paddingLeft: 10,
+        paddingRight: 10
+    },
+
+    StatusText: {
+        textAlign: 'center',
+        fontSize: 20,
+    },
+
+    StatusImg: {
+        marginLeft: 10,
+        width: 20,
+        height: 20,
+    }
+});
+
+export default compose() (StatusText)
--- src/screens/ViewProfile.js
+++ src/screens/ViewProfile.js
@@ -1,19 +1,18 @@
 import React from 'react';
 import compose from 'recompose/compose'
 import PropTypes from 'prop-types'
-import {StyleSheet, Image, View, Text, Picker, PickerIOS} from "react-native";
+import {StyleSheet, Image, View, Text} from "react-native";
+import StatusText from '../components/StatusText';
 
 class ViewProfile extends React.Component {
 
-    static propTypes = {
-
-    }
+    static propTypes = {}
 
     state = {
         imgUrl: "https://st2.depositphotos.com/1006318/10458/v/950/depositphotos_104583834-stock-illustration-business-man-profile-icon-male.jpg",
         fullname: "FULLNAME",
-        description: "DESCRIPTION",
-        status: ["Looking for job", "Looking for opportunity", "Not looking for job"],
+        description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+        status: "Looking for job",
         experience: [
             {
                 title: "EXPERIENCE 1",
@@ -28,32 +27,29 @@ class ViewProfile extends React.Component {
                 date: ""
             }
         ],
-        currentStatus: ""
+
     };
 
-    render(){
-        const {imgUrl, fullname, status, currentStatus} = this.state;
-
-        let statusItems = [];
-
-        status.forEach((value, index) => {
-            statusItems.push(<Picker.Item label={value} value={value}/>)
-        });
+    render() {
+        const {imgUrl, fullname, status, description} = this.state;
 
         return (
             <View style={styles.MainContainer}>
+
                 <Image source={{uri: imgUrl}}
-                       style={{width: 150, height: 150, borderRadius: 150/2}}
+                       style={{width: 150, height: 150, borderRadius: 150 / 2}}
                 />
+
                 <Text style={styles.ProfileName}>
                     {fullname}
                 </Text>
-                <Picker
-                    selectedValue={currentStatus}
-                    style={styles.StatusPicker}
-                    onValueChange={(itemValue, itemIndex) => this.setState({currentStatus: itemValue})}>
-                    {statusItems}
-                </Picker>
+
+                <StatusText status={status}/>
+
+                <Text style={styles.Description}>
+                    {description}
+                </Text>
+
             </View>
         )
     }
@@ -64,7 +60,8 @@ const styles = StyleSheet.create({
     MainContainer: {
         flex: 1,
         alignItems: 'center',
-        justifyContent: 'center'
+        justifyContent: 'center',
+
     },
 
     ProfileName: {
@@ -72,11 +69,11 @@ const styles = StyleSheet.create({
         fontSize: 26,
     },
 
-    StatusPicker: {
-        height: 50,
-        width: 200,
+    Description: {
+        marginTop: 20,
+        maxWidth: '90%',
+        textAlign: 'center'
     }
-
 });
 
-export default compose() (ViewProfile)
+export default compose()(ViewProfile)
