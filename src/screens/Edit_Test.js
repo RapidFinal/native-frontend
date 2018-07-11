a25cad6ecf4037ad9b3520c2ff90fb3aba54704a
Sky <dodinhtrung97@gmail.com>
2018-07-11 07:30:55 +0700

    Edit button

--- src/screens/Edit_Test.js
+++ src/screens/Edit_Test.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import compose from 'recompose/compose'
 import PropTypes from 'prop-types'
-import {Button, StyleSheet, Text, ScrollView, TouchableOpacity} from "react-native";
-import {TextField} from 'react-native-material-textfield';
+import { StyleSheet, Text, ScrollView, TouchableOpacity } from "react-native";
+import { TextField } from 'react-native-material-textfield';
+import { Button } from 'react-native-elements';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 class Edit_Test extends React.Component {
 
@@ -60,13 +62,17 @@ class Edit_Test extends React.Component {
             <TextField
               label = "Other"
               onChangeText = { this.handleVideoUrlChange } />
-            <Button
-              style = { styles.button }
-              title = "Save"
-              onPress = {
-                () => console.log("Hello")
-              }>
-            </Button>
+
+            <ScrollView contentContainerStyle = { styles.buttonContainer }>
+              <TouchableOpacity
+                style = { styles.button }
+                onPress = {
+                  () => console.log("Hello")
+                }>
+                <Icon name={"edit"} size={30} />
+              </TouchableOpacity>
+            </ScrollView>
+
           </ScrollView>
         );
     }
@@ -79,16 +85,26 @@ class Edit_Test extends React.Component {
 
 const styles = StyleSheet.create({
     container: {
-      flex: 1,
-      marginLeft: 30,
-      marginRight: 30,
+      marginLeft: 20,
+      marginRight: 20,
+    },
+    buttonContainer: {
+      alignItems:'center',
+      justifyContent:'center',
+      marginLeft: 20,
+      marginRight: 20,
+      marginTop: 30,
+      marginBottom: 30,
     },
     button: {
-      width: 30,
-      justifyContent: 'center',
-      alignItems: 'center',
-      backgroundColor: '#2679ff',
-      marginTop: 20
+      borderWidth:1,
+      borderColor:'rgba(0,0,0,0.2)',
+      alignItems:'center',
+      justifyContent:'center',
+      width:70,
+      height:70,
+      backgroundColor:'#fff',
+      borderRadius:70,
     }
 });
 
