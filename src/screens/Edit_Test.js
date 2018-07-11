0b68a1aa73afce52ca11ea2e3b50472aa154e269
Sky <dodinhtrung97@gmail.com>
2018-07-11 08:23:48 +0700

    Finished all required fields, want to incorperate date picker and revamp textinput

--- src/screens/Edit_Test.js
+++ src/screens/Edit_Test.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import compose from 'recompose/compose'
 import PropTypes from 'prop-types'
-import { StyleSheet, Text, ScrollView, TouchableOpacity } from "react-native";
+import { View, StyleSheet, Text, ScrollView, TouchableOpacity, TextInput } from "react-native";
 import { TextField } from 'react-native-material-textfield';
-import { Button } from 'react-native-elements';
+import { Button, Divider } from 'react-native-elements';
 import Icon from 'react-native-vector-icons/FontAwesome';
 
 class Edit_Test extends React.Component {
@@ -46,26 +46,55 @@ class Edit_Test extends React.Component {
 
     render(){
         return (
-          <ScrollView contentContainerStyle = { styles.container }>
-            <TextField
-              label = "Project Name"
-              onChangeText = { this.handleNameChange } />
-            <TextField
-              label = "Project Description"
-              onChangeText = { this.handleDescriptionChange } />
-            <TextField
-              label = "Date"
-              onChangeText = { this.handleDateChange } />
-            <TextField
-              label = "Github"
-              onChangeText = { this.handleGitUrlChange } />
-            <TextField
-              label = "Other"
-              onChangeText = { this.handleVideoUrlChange } />
+          <ScrollView contentContainerStyle = {styles.container}>
+            // Project Name
+            <View style = {styles.inputContainer}>
+              <Text style = {styles.inputTitle}> Project Name </Text>
+              <TextInput style = {styles.input}
+               autoCapitalize = "none"
+               onChangeText = {this.handleNameChange}/>
+            </View>
+            // Project Description
+            <View style = {styles.inputContainer}>
+              <Text style = {styles.inputTitle}> Project Description </Text>
+              <TextInput style = {styles.inputLong}
+               autoCapitalize = "none"
+               onChangeText = {this.handleDescriptionChange}/>
+            </View>
+            // Project Date
+            <View style = {styles.inputContainer}>
+              <Text style = {styles.inputTitle}> Date </Text>
+              <TextInput style = {styles.input}
+               autoCapitalize = "none"
+               onChangeText = {this.handleDateChange}/>
+            </View>
 
-            <ScrollView contentContainerStyle = { styles.buttonContainer }>
+            <Divider style = {{marginTop: 15, marginBottom: 15, backgroundColor: 'rgba(0,0,0,0.2)'}}/>
+
+            // Github URL
+            <View style = {styles.inputContainer}>
+              <View style = {styles.titleWithIconContainer}>
+                <Text style = {styles.inputTitle}> Github </Text>
+                <Icon name={"github"} size={20}/>
+              </View>
+              <TextInput style = {styles.input}
+               autoCapitalize = "none"
+               onChangeText = {this.handleGitUrlChange}/>
+            </View>
+            // Video URL
+            <View style = {styles.inputContainer}>
+              <View style = {styles.titleWithIconContainer}>
+                <Text style = {styles.inputTitle}> Video </Text>
+                <Icon name={"youtube"} size={20}/>
+              </View>
+              <TextInput style = {styles.input}
+               autoCapitalize = "none"
+               onChangeText = {this.handleVideoUrlChange}/>
+            </View>
+
+            <ScrollView contentContainerStyle = {styles.buttonContainer}>
               <TouchableOpacity
-                style = { styles.button }
+                style = {styles.button}
                 onPress = {
                   () => console.log("Hello")
                 }>
@@ -85,6 +114,7 @@ class Edit_Test extends React.Component {
 
 const styles = StyleSheet.create({
     container: {
+      marginTop: 20,
       marginLeft: 20,
       marginRight: 20,
     },
@@ -95,6 +125,30 @@ const styles = StyleSheet.create({
       marginRight: 20,
       marginTop: 30,
       marginBottom: 30,
+    },
+    inputContainer: {
+      marginTop: 10
+    },
+    input: {
+      margin: 15,
+      height: 40,
+      borderColor: 'rgba(0,0,0,0.2)',
+      backgroundColor:'#fff',
+      borderWidth: 1
+    },
+    inputLong: {
+      margin: 15,
+      height: 120,
+      borderColor: 'rgba(0,0,0,0.2)',
+      backgroundColor:'#fff',
+      borderWidth: 1
+    },
+    inputTitle: {
+      fontWeight: 'bold',
+      fontSize: 14
+    },
+    titleWithIconContainer: {
+      flexDirection: 'row'
     },
     button: {
       borderWidth:1,
