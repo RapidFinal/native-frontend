54aece3a00858eea64d2cd1f44f5053583668740
karn806 <nattakarn.kl@gmail.com>
2018-07-12 21:45:42 +0700

    fixed navigating

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -23,7 +23,7 @@ class Home extends React.Component {
     }
 
     goToProfile = (userID) => {
-        // this.props.navigation.navigate("Profile", { userID: userID });
+        this.props.navigation.navigate("ViewProfile", { userID: userID });
     };
 
     onChangeText = (text) => {
