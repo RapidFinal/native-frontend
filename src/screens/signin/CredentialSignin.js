75aae3e23aa40234d778e9af3f57ad5340f8dcfd
hart_suvanadat@hotmail.com <hart_suvanadat@hotmail.com>
2018-07-15 22:14:13 +0700

    Now redirect correctly to correct stack based on user role.

--- src/screens/signin/CredentialSignin.js
+++ src/screens/signin/CredentialSignin.js
@@ -48,9 +48,6 @@ class CredentialSignin extends React.Component {
         const {email, password} = this.state;
         try {
             const auth = await CredentialAuthentication.signin({email, password})
-            if (auth !== null){
-                this.props.navigation.navigate("MainEmployer")
-            }
         } catch (e) {
 
             console.log(e.code)
