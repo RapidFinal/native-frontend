ddfbe64f1023fb108a888291fa3f402e526e5d5a
Peter Son Struschka <peter.struschka@gmail.com>
2018-07-16 11:05:06 +0700

    Search from SearchResults screen

--- src/api/search/Search.js
+++ src/api/search/Search.js
@@ -13,13 +13,12 @@ const cleanString = (str) => {
 // Takes array of tags that matched, and the snapshot of the tags object
 const searchByTag = async (tags, snapshot) => {
     const filtered = _.filter(snapshot, tag => tags.includes(tag.tagName));
-    console.log(filtered);
+    //console.log(filtered);
     const arr = _.map(filtered, e => (e.hasOwnProperty("employeeIds") ? Object.keys(e.employeeIds) : []));
-    console.log(arr);
+    //console.log(arr);
     const ids = _.uniq(_.reduce(arr, (a, n) => a.concat(n),[]));
-    console.log('ids', ids);
-
-    console.log('s', snapshot);
+    //console.log('ids', ids);
+    //console.log('s', snapshot);
     
     let db = new DatabaseService;
 
@@ -27,17 +26,17 @@ const searchByTag = async (tags, snapshot) => {
         var info = await db.getEmployeeInfo(id);
         info.tags = info.tagIds.map( id => snapshot[id].tagName );
 	info.uid = id;
-	console.log(id, '->', info);
+	//console.log(id, '->', info);
 	
         return info;
     }));
 
-    console.log('infos:', infos);
+    //console.log('infos:', infos);
     return infos;
 };
 
 const searchByName = async (name) => {
-    let db = new DatabaseService;
+    //let db = new DatabaseService;
     console.log(name);
     return [];
 };
@@ -49,7 +48,7 @@ export const search = async (searchString) => {
     const tags = _.map(snap, e => e.tagName );
 
     const tokens = searchString.split(" ");
-    console.log(tokens, "---");
+    //console.log(tokens, "---");
 
     const tagMatches = _.intersectionBy(tags, tokens, cleanString);
 
