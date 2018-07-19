fdac37c4d3ac635113ecb5d9a641107156874d85
Soraya Sakonwittayanon <soraya.sakonwittayanon@gmail.com>
2018-07-19 15:51:47 +0700

    update and get employer recent view

--- src/api/databaseService.js
+++ src/api/databaseService.js
@@ -469,6 +469,49 @@ class DatabaseService {
     });
   }
 
+  updateEmployerRecentView(uid, recentViewUid) {
+    this.getEmployerRecentView(uid).then(recentViews => {
+      let val = [];
+      if (recentViews === []) {
+        val.push(recentViewUid);
+        firebase.database().ref("employerInfo/" + uid + "/recentViews/").set(val);
+      } else {
+        val = recentViews.reverse();
+        if (recentViews.length < 5){
+          if (recentViews.includes(recentViewUid)){
+            let index = recentViews.indexOf(recentViewUid);
+            val.splice(index, 1);
+            val.push(recentViewUid)
+          } else {
+            val.push(recentViewUid);
+          }
+        } else {
+          if (recentViews.includes(recentViewUid)){
+            let index = recentViews.indexOf(recentViewUid);
+            val.splice(index, 1);
+            val.push(recentViewUid)
+          } else {
+            val.splice(0, 1);
+            val.push(recentViewUid);
+          }
+        }
+        firebase.database().ref("employerInfo/" + uid + "/recentViews/").set(val.reverse());
+      }
+    })
+  }
+
+  getEmployerRecentView(uid) {
+    return new Promise((resolve, reject) => {
+      firebase.database().ref("employerInfo/" + uid + "/recentViews/").once('value').then(function(snapshot) {
+        if (snapshot.val() === null){
+          resolve([])
+        } else {
+          resolve(snapshot.val())
+        }
+      });
+    });
+  }
+
   // ret = {firstName: "Alice", lastName:"Smith", companyName: "MUIC", imgUrl,
   //   categories: [
   //     {
